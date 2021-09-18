c2098f4135f7be7a4996482ef1f6a254f3fb7f6f
Zhiozhio <zhijun_lyu@163.com>
2021-09-18 10:58:37 +0800

    Little change of load_map.cpp

--- load_map.cpp
+++ load_map.cpp
@@ -66,54 +66,15 @@ void rungui(SurfelMapping & core, GUI & gui)
             }
 
 
+            //float backColor[4] = {0.05, 0.05, 0.3, 0.0f};
+            float backColor[4] = {0, 0, 0, 0};
+            gui.preCall(backColor);
+
             //====== Enter Path Mode until Complete
             bool initView = true;
             int totalNovelViewNum = 0;
-            while(gui.pathMode->Get())
+            if(gui.pathMode->Get())
             {
-                float backColor[4] = {0, 0, 0, 0};
-                gui.preCall(backColor);
-
-                /*
-                //=== overlook tansform
-                pangolin::OpenGlMatrix mv;
-
-                Eigen::Matrix3f currRot;
-                currRot = Eigen::AngleAxis<float>(-M_PI_2, Eigen::Vector3f(1, 0, 0));
-
-                Eigen::Vector3f forwardVector(0, 0, 1);
-                Eigen::Vector3f upVector(0, -1, 0);
-
-                Eigen::Vector3f forward = (currRot * forwardVector).normalized();
-                Eigen::Vector3f up = (currRot * upVector).normalized();
-
-                Eigen::Vector3f viewAt;
-                if(initView)
-                {
-                    viewAt = Eigen::Vector3f(pose(0, 3), -15, pose(2, 3));
-                }
-                else
-                {
-                    pangolin::OpenGlMatrix currMVInv = gui.s_cam.GetModelViewMatrix().Inverse();
-                    viewAt = Eigen::Vector3f(currMVInv(0, 3), currMVInv(1, 3), currMVInv(2, 3));
-                }
-
-                Eigen::Vector3f eye = viewAt;
-
-                Eigen::Vector3f z = -forward;  // Forward, OpenGL camera z direction
-                Eigen::Vector3f x = up.cross(z).normalized();     // Right
-                Eigen::Vector3f y = z.cross(x);                   // Up
-
-                Eigen::Matrix4d m;                                // [R; U; F]_4x4 * [E; -eye]_4x4
-                m << x(0),  x(1),  x(2),  -(x.dot(eye)),
-                        y(0),  y(1),  y(2),  -(y.dot(eye)),
-                        z(0),  z(1),  z(2),  -(z.dot(eye)),
-                        0,     0,     0,      1;
-
-                memcpy(&mv.m[0], m.data(), sizeof(Eigen::Matrix4d));
-
-                gui.s_cam.SetModelViewMatrix(mv);
-                */
 
                 //=== draw all history frame
                 std::vector<Eigen::Vector3f> posVerts, posVertNovel;
@@ -141,18 +102,6 @@ void rungui(SurfelMapping & core, GUI & gui)
                     glColor3f(1.0f,1.0f,1.0f);
                 }
 
-                //=== draw model
-                core.getGlobalModel().renderModel(gui.s_cam.GetProjectionModelViewMatrix(),
-                                                  gui.s_cam.GetModelViewMatrix(),
-                                                  0.0,
-                                                  true,
-                                                  false,
-                                                  true,
-                                                  false,
-                                                  false,
-                                                  false,
-                                                  3,
-                                                  3);
 
                 //=== If acquire images
                 if(pangolin::Pushed(*gui.acquirePairedImage))
@@ -291,15 +240,11 @@ void rungui(SurfelMapping & core, GUI & gui)
                 }
 
 
-                gui.postCall();
-
-                initView = false;
             }
 
 
 
-            float backColor[4] = {0.05, 0.05, 0.3, 0.0f};
-            gui.preCall(backColor);
+
 
             //============ draw global model
             int surfel_mode = gui.drawGlobalModel->Get();
